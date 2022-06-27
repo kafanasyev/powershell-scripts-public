15f25811f2023c4ddc71e8ba75e16172bb15d1b2
kafanasyev <proak4@gmail.com>
2022-06-27 10:44:23 -0400

    1st try

---
+++ manipulating jsons.ps1
@@ -0,0 +1,16 @@
+﻿$path = "C:\TestData\PowerKI-WeeklyData2"
+$path2 = "C:\TestData\PowerKI-WeeklyData2\Converted"
+cd $path
+
+$files = Get-Content -Path "*.json" -Raw
+
+foreach($f in $files){
+$FileContent = $f | ConvertFrom-Json
+$id = $FileContent.transactionId
+$FileContent = $FileContent | Select-Object * -ExcludeProperty _rid
+$FileContent = $FileContent | Select-Object * -ExcludeProperty _self
+$FileContent = $FileContent | Select-Object * -ExcludeProperty _etag
+$FileContent = $FileContent | Select-Object * -ExcludeProperty _attachments
+$FileContent = $FileContent | Select-Object * -ExcludeProperty _ts
+$FileContent | ConvertTo-Json | Out-File "$path2\$id.json"
+}
---
+++ manipulating transactions.ps1
@@ -0,0 +1,69 @@
+﻿$path = "C:\TestData\6-15-2022-Databricks test\793-6-12\793"
+$path2 = "C:\TestData\6-15-2022-Databricks test\793-6-12\793\Converted"
+cd $path
+$changeDateTo = "2022-06-11"
+$stopAfter = 2000
+
+$files = Get-Content -Path "*.txt" -Raw
+
+$i = 0
+
+foreach($f in $files){
+
+    if($i -eq $stopAfter){
+       break
+    } 
+
+    #Write-Host $f
+
+    [xml]$xml = $f
+    Write-Host $xml
+    $id = "test.xml"
+    try{
+        $nodes =  $xml.SelectNodes("//PublixPay")
+        $id = $nodes.PublixPayQRCode           
+    }catch{
+        Write-Host "1st try catch failed"
+    }
+
+    
+    try{
+        $nodes =  $xml.SelectNodes("//TicketStart")
+        foreach ($node in $nodes) {        
+            $node.BusinessDate = $changeDateTo
+        }    
+    }catch{
+     Write-Host "2nd try catch failed"
+    }
+
+    try{
+        $nodes =  $xml.SelectNodes("//Trailer")
+        foreach ($node in $nodes) {        
+            $node.ReceiptDate = $changeDateTo
+        }    
+    }catch{
+     Write-Host "2nd try catch failed"
+    }
+
+     try{
+        $nodes =  $xml.SelectNodes("//TicketEnd")
+        foreach ($node in $nodes) {        
+            $node.BusinessDate = $changeDateTo
+        }    
+    }catch{
+     Write-Host "2nd try catch failed"
+    }
+
+    Write-Host $id 
+    $id2 = $id.Substring(0,$id.Length-1)
+    Write-Host $id2 
+    $path3 = $path2 + "\" + $id2 + "-12345678-123456.txt";    
+    Write-Host $path3
+
+    $xml.Save($path3)
+    $i++;
+}
+
+
+
+
